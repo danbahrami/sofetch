6b593616dded288059e4583e4b7421d66caa6c85
Dan Bahrami <dan.bahrami@googlemail.com>
2024-09-08 17:49:20 +0200

    Publicly export useful types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ export { createClient } from "@/createClient";
 export const f = createClient();
 
 export { HttpError, NetworkError, JsonParseError, JsonStringifyError } from "@/errors";
+export type {
+    SoFetchClient,
+    SoFetchClientOptions,
+    DecoratedResponse,
+    DecoratedResponsePromise,
+    Callbacks,
+} from "@/types";
